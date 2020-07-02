1f271a5fbda4c28f9828af9cd8d8152d5d28e727
Hyung Kim <hjkmines@gmail.com>
2020-07-02 01:41:06 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,10 +9,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
+  "SELECT avg(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
+  "SELECT sum(tardies) FROM students;"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT avg(gpa) FROM students WHERE grade = 9;"
 end
